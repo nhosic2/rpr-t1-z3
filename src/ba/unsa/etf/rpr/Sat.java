3fe81fc20fbc377047fc64c9f4f7ffcdbd8daf59
Nejra Hosic <nhosic2@etf.unsa.ba>
2020-10-26 16:58:35 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,70 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    private int sati, minute, sekunde;
+
+    public Sat(int sati, int minute, int sekunde) {
+        postavi(sati, minute, sekunde);
+    }
+
+    void postavi(int sati, int minute, int sekunde) {
+        this.sati = sati;
+        this.minute = minute;
+        this.sekunde = sekunde;
+    }
+
+    void sljedeci() {
+        sekunde=minute+1;
+        if (sekunde == 60) {
+            sekunde = 0;
+            minute=minute+1;
+        }
+        if (minute == 60) {
+            minute = 0;
+            sati=sati+1;
+        }
+        if (sati == 24) sati = 0;
+    }
+
+    void prethodni() {
+        sekunde=sekunde-1;
+        if (sekunde == -1) {
+            sekunde = 59;
+            minute=minute-1;
+        }
+        if (minute == -1) {
+            minute = 59;
+            sati=sati-1;
+        }
+        if (sati == -1) sati = 23;
+    }
+
+    void pomjeriZa(int pomak) {
+        if (pomak > 0) for (int i =0; i < pomak ; i++) sljedeci();
+        else for (int i=0; i < -pomak ; i++) prethodni();
+    }
+
+    int getSate()
+
+    {
+        return sati;
+    }
+
+    int getMinute()
+
+    {
+        return minute;
+    }
+
+    int getSekunde()
+
+    {
+        return sekunde;
+    }
+
+    void Ispisi()
+
+    {
+        System.out.println(""+sati+":"+minute+":"+sekunde);
+    }
+}
